decf9429171c69d3f6aadfba3f32fce58325440d
anonymousEROS <dovala69@gmail.com>
2025-09-21 09:54:09 -0700

    DS debate

--- getData.py
+++ getData.py
@@ -2,37 +2,33 @@ import wikipedia
 import os
 
 
-#print(wikipedia.search("neural network"))
- 
-# NN = wikipedia.page("neural network(machine learning)")
-
-# print(NN.content)
-
-
-
-
 def getData():
-    # data = []
+
+    data = []
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, 'topics.txt')
     
-    #with open(file_path) as f:
-       # lines = [line.rstrip('\n') for line in f]
-            
-        #print(lines)
     file = open(file_path, 'r')
     while True:
         next_line = file.readline()
-
         if not next_line:
             break
         l = next_line.strip()
-        print(l)
+    data.append(l)
+    print(data)  
+   # try:
+   #     print(l)
+   #     data = wikipedia.page(l)
+   #     print(data.title)
+   #     dataContent = data.content
+   #     print(dataContent)
+   # except wikipedia.exceptions.DisambiguationError as e:
+   #     print(e.options)
+    
 
 
 if __name__ == "__main__":
     
     getData()
-
 
 
