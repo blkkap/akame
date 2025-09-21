814cb5ddae80f38ab5940149a1810e4bd64fbaef
anonymousEROS <dovala69@gmail.com>
2025-09-20 22:38:49 -0700

    back to the basics

--- getData.py
+++ getData.py
@@ -1,8 +1,38 @@
 import wikipedia 
+import os
 
-print(wikipedia.search("neural network"))
+
+#print(wikipedia.search("neural network"))
  
-NN = wikipedia.page("neural network(machine learning)")
+# NN = wikipedia.page("neural network(machine learning)")
 
-print(NN.content)
+# print(NN.content)
+
+
+
+
+def getData():
+    # data = []
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, 'topics.txt')
+    
+    #with open(file_path) as f:
+       # lines = [line.rstrip('\n') for line in f]
+            
+        #print(lines)
+    file = open(file_path, 'r')
+    while True:
+        next_line = file.readline()
+
+        if not next_line:
+            break
+        l = next_line.strip()
+        print(l)
+
+
+if __name__ == "__main__":
+    
+    getData()
+
+
 
