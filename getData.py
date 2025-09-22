5022b67cf5abb1a4ebe16ee931bd2a43c85af46e
anonymousEROS <dovala69@gmail.com>
2025-09-22 13:28:22 -0700

    need neural

--- getData.py
+++ getData.py
@@ -3,8 +3,15 @@ from bs4 import BeautifulSoup
 import os
 
 
+
 def getData():
-    url = "https://en.wikipedia.org/wiki/Artificial_intelligence"
+
+    with open("topics.txt", 'r', encoding='UTF-8') as file:
+        while line := file.readline():
+           # l = line.rstrip()
+
+            url = line.rstrip()
+    
     headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     
@@ -13,21 +20,21 @@ def getData():
     soup = BeautifulSoup(response.content, 'html.parser')
    
     title = soup.find(id="firstHeading").text
-    print(title)
+   # print(title)
 
     paragraphs = soup.select("p")
     for p in paragraphs:
-        print(p.text)
+        #print(p.text)
 
-    p_text = '\n'.join(para.text for para in paragraphs[:])
-    with open('data/p_text', 'w', encoding='utf-8') as file:
-        file.write(p_text)
+        p_text = '\n'.join(para.text for para in paragraphs[:])
+    with open(f'data/{title}', 'w', encoding='utf-8') as file:
+        file.write(p_text)    
     
     
 
 
 if __name__ == "__main__":
-    
     getData()
+    
 
 
