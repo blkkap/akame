6c0f6011de1803251b53e7aadc700b7142f287b7
anonymousEROS <dovala69@gmail.com>
2025-09-21 21:24:50 -0700

    back to the basics.. i hate this

--- getData.py
+++ getData.py
@@ -4,28 +4,22 @@ import os
 
 def getData():
 
-    data = []
+   # data = []
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, 'topics.txt')
     
-    file = open(file_path, 'r')
-    while True:
-        next_line = file.readline()
-        if not next_line:
-            break
-        l = next_line.strip()
-    data.append(l)
-    print(data)  
-   # try:
-   #     print(l)
-   #     data = wikipedia.page(l)
-   #     print(data.title)
-   #     dataContent = data.content
-   #     print(dataContent)
-   # except wikipedia.exceptions.DisambiguationError as e:
-   #     print(e.options)
-    
-
+    with open(file_path, 'r') as f:
+        topics= [line.strip() for line in f if line.strip()]
+             
+        try:
+            for topic in topics:
+                print(topic)
+                data =  wikipedia.page(topic)
+                print(data.title)
+                dataContent = data.content
+                print(dataContent)
+        except wikipedia.exceptions.DisambiguationError as e:   
+            print(e.options)
 
 if __name__ == "__main__":
     
