903c7d9c90c757ed8d59af87d17edc087b0634f6
anonymousEROS <dovala69@gmail.com>
2025-09-22 14:03:07 -0700

    knowledge

--- getData.py
+++ getData.py
@@ -3,37 +3,39 @@ from bs4 import BeautifulSoup
 import os
 
 
-
 def getData():
 
     with open("topics.txt", 'r', encoding='UTF-8') as file:
-        while line := file.readline():
-           # l = line.rstrip()
-
-            url = line.rstrip()
+        urls = [line.rstrip() for line in file if line.rstrip()]
     
-    headers = {'User-Agent':
-               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    for url in urls:
+        try:
+            
+            #url = "url here" 
     
-    response = requests.get(url, headers=headers)
+            headers = {'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     
-    soup = BeautifulSoup(response.content, 'html.parser')
+            response = requests.get(url, headers=headers)
+    
+            soup = BeautifulSoup(response.content, 'html.parser')
    
-    title = soup.find(id="firstHeading").text
+            title = soup.find(id="firstHeading").text
    # print(title)
 
-    paragraphs = soup.select("p")
-    for p in paragraphs:
+            paragraphs = soup.select("p")
+            for p in paragraphs:
         #print(p.text)
 
-        p_text = '\n'.join(para.text for para in paragraphs[:])
-    with open(f'data/{title}', 'w', encoding='utf-8') as file:
-        file.write(p_text)    
-    
-    
+                p_text = '\n'.join(para.text for para in paragraphs[:])
+                with open(f'data/{title}', 'w', encoding='utf-8') as file:
+                    file.write(p_text)    
+        except Exception as e:
+            print(f"error fetching {url}: {e}")
 
 
 if __name__ == "__main__":
+    
     getData()
     
 
